92939fe6c6861d639139f2ad2a3636b09b1df6c3
Blubu <TeX@mareipeischl.de>
2017-10-02 17:04:09 +0200

    bett temperatur angeglichen für erste schicht

--- firmware/Marlin/Configuration.h
+++ firmware/Marlin/Configuration.h
@@ -173,7 +173,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 // The position of the homing switches. Use MAX_LENGTH * -0.5 if the center should be 0, 0, 0
 #define X_HOME_POS (X_MIN_POS - 1)  
 #define Y_HOME_POS (Y_MIN_POS - 1)
-#define Z_HOME_POS (196.60)
+#define Z_HOME_POS (196.70)
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
 #define Y_MAX_LENGTH (Y_MAX_POS - Y_MIN_POS)
